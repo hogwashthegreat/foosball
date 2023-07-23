e7c145350c8835c77c8f04abad83edca6ba39ee8
hogwashthegreat <ayden.tan@live.com>
2023-07-23 15:44:00 -0700

    Handler comments

--- Handler.py
+++ Handler.py
@@ -7,9 +7,11 @@ import numpy as np
 class Handler:
     def __init__(self):
         self.shutdown_event = threading.Event()
-        self.processed_frames = 0
-        self.centers = np.zeros((3,2))
+        self.processed_frames = 0 #test var to check how many frames are being processed to calculate fps
+        self.centers = np.zeros((3,2)) #array of tuples with array index 0 being most recent and tuple is (x,y)
+
     def __call__(self, cam: Camera, stream: Stream, frame: Frame):
+        #waitkey for opencv stream
         ENTER_KEY_CODE = 13
 
         key = cv2.waitKey(1)
@@ -17,47 +19,32 @@ class Handler:
            self.shutdown_event.set()
            return
         
-
+        #frame processing
         if frame.get_status() == FrameStatus.Complete:
-            #print('{} acquired {}'.format(cam, frame), flush=True)
-            display = frame.as_opencv_image()
-            #display = display[:500, :500]
+            display = frame.as_opencv_image() #convert vimba to opencv format
             
             #Get new ball center and update array
             center, mask = helper.getBallCenter(display)
             self.centers[2] = self.centers[1]
             self.centers[1] = self.centers[0]
             self.centers[0] = center
-            #print(self.centers)
-            
-            '''
-            #get predicted next position and draw it (if centers found)
-            try:
-                nextPos, velo = helper.getNextPos(self.centers)
-                cv2.circle(display, nextPos, 4 ,(0,255,0), -1)
-                print(self.centers,nextPos)
-                #print(nextPos)
-            except:
-                pass
-            '''
-            #Only gets and displays first 1000 frames
-            msg = 'Stream from \'{}\'. Press <Enter> to stop stream.'            
+                        
             
             scale_percent = 50 # percent of original size
             width = int(display.shape[1] * scale_percent / 100)
             height = int(display.shape[0] * scale_percent / 100)
             dim = (width, height)
-            display = cv2.resize(display, dim, interpolation = cv2.INTER_AREA)
-            mask  = cv2.resize(mask, dim, interpolation = cv2.INTER_AREA)
-            cv2.imshow(msg.format(cam.get_name()), display)
-            cv2.imshow("1", mask)
-            self.processed_frames += 1
-            if self.processed_frames >= 10000:
+            display = cv2.resize(display, dim, interpolation = cv2.INTER_AREA) #resize original image
+            mask  = cv2.resize(mask, dim, interpolation = cv2.INTER_AREA) #resize mask
+            cv2.imshow("Camera", display) 
+            cv2.imshow("Mask", mask)
+            self.processed_frames += 1 #Frame has been processed
+            if self.processed_frames >= 10000: #if 10k frames are processed end the program (for testing)
                 self.shutdown_event.set()
                 return
             
 
-        cam.queue_frame(frame)
+        cam.queue_frame(frame) #get next frame from buffer
         
-    def getFrames(self):
+    def getFrames(self): #return number of processed frames (for fps)
         return self.processed_frames
