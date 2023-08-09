5a4ed4bd2a7c7f1c9fda55b9fc140b3735457754
bonoboTheShrimp <rahulsundaresan@outlook.com>
2023-08-09 11:42:19 -0700

    motor testing

--- arduinotest.py
+++ arduinotest.py
@@ -50,33 +50,58 @@ import time
 
 board = pyfirmata.Arduino("COM5")
 steps = 200
-rotations = 4
+rotations = 2
 times = 2
 delayTime = .0000001*1
 print("hi")
 
 dirPin = board.get_pin("d:9:o")
 stepPin = board.get_pin("d:6:o")
-print("pins")
-def rotate(steps):
-    print("rotate")
+def sleep(duration):
+    start = time.perf_counter_ns()
+    while True:
+        elapsed = time.perf_counter_ns()-start
+        remaining = duration-elapsed
+        if remaining <= 0:
+            break
+        if remaining > 0.02:
+            time.sleep(max(remaining/2, 0.0001))
+        else:
+            pass
+def rotate(steps, direciton):
+    dirPin.write(direciton)
     for a in range(steps):
         stepPin.write(1)
-        #time.sleep(delayTime)
+
         stepPin.write(0)
-        #time.sleep(delayTime)
+
     
-while True:
-    for x in range(times):
-        for y in range(rotations):
-            dirPin.write(0)
-            rotate(steps)
-        time.sleep(0.05)
-        for z in range(rotations):
-            dirPin.write(1)
-            rotate(steps)
-        print("done")    
-    break
+"""
+for x in range(times):
+    for y in range(rotations):
+        dirPin.write(1)
+        rotate(50)
+    time.sleep(0.05)
+"""
+
+input("")
+
+rotate(400, 0)
+rotate(200, 1) #200
+rotate(322, 0) #522
+rotate(144, 0) #666
+rotate(555, 1) #111
+rotate(700, 0) #811
+rotate(10, 0) #821
+rotate(32, 1) #789
+rotate(776, 1) #13
+rotate(100, 0) #113
+rotate(660, 0) #773
+rotate(222, 1) #551
+rotate(551, 1) #0
+
+
+
     
 
     
