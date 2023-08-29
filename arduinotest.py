2a1f62ede952fa0c7961686873cf42753f3c41b4
hogwashthegreat <ayden.tan@live.com>
2023-08-29 13:04:13 -0700

    multi motor arduino

--- arduinotest.py
+++ arduinotest.py
@@ -11,41 +11,36 @@ rotations = 2
 times = 2
 print("hi")
 
-dirPin = board.get_pin("d:9:o")
-stepPin = board.get_pin("d:6:o")
+dirPin = [board.get_pin("d:9:o"), board.get_pin("d:10:o")]
 
-def rotate(steps, direction):
-    dirPin.write(direction)
+stepPin = [board.get_pin("d:6:o"), board.get_pin("d:5:o")]
+
+
+def rotate(steps, direction, motor):
+    dirPin[motor].write(direction)
     for a in range(steps):
-        stepPin.write(1)
-        stepPin.write(0)
+        stepPin[motor].write(1)
+        stepPin[motor].write(0)
 
     
-import keyboard  # using module keyboard
+import keyboard  # using module keyboardljljljlj
 while True:  # making a loop
     try:  # used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed('a'):  # if key 'q' is pressed 
-            rotate(1,0)
+        if keyboard.is_pressed("a"):  # if key 'q' is pressed 
+            rotate(1,0,0)
               # finishing the loop
         elif keyboard.is_pressed("d"):
-            rotate(1,1)
+            rotate(1,1,0)
+        elif keyboard.is_pressed("j"):  # if key 'q' is pressed 
+            rotate(1,0,1)
+              # finishing the loop
+        elif keyboard.is_pressed("l"):
+            rotate(1,1,1)
     except:
         break  # if user pressed a key other than the given key the loop will break
 
 input("")
-rotate(400, 0)
-rotate(200, 1) #200
-rotate(322, 0) #522
-rotate(144, 0) #666
-rotate(555, 1) #111
-rotate(700, 0) #811
-rotate(10, 0) #821
-rotate(32, 1) #789
-rotate(776, 1) #13
-rotate(100, 0) #113
-rotate(660, 0) #773
-rotate(222, 1) #551
-rotate(551, 1) #0
+
 
 
 
